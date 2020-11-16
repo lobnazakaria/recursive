abb6abf2f7c7992df22ae4c1aff7d6b0542ebbd7
lobna <lobna@seamlabs.com>
2020-11-16 12:28:12 +0200

    recurseve template and function

--- recursiveApp/src/app/app.component.ts
+++ recursiveApp/src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JsonFilesService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'recursiveApp';
+export class AppComponent implements OnInit {
+  data = [];
+  panelOpenState = false;
+  n;
+  r;
+  value;
+  constructor(private dataService: JsonFilesService){}
+  ngOnInit(){
+    this.getData();
+  }
+  getData(){
+    this.dataService.getData().subscribe((res: any) => {
+      this.data = res;
+      console.log(this.data);
+    })
+  }
+  permutationValue(){
+    let cpyN = this.n;
+    let cpyR = this.r;
+    let facN = cpyN;
+    let NsubR =  cpyN - cpyR;
+    let facNsubR = NsubR;
+    if((this.n < this.r) || (this.n == 0) || (this.r == 0)){
+      this.value="n must be above r and both must be above 0";
+    } else {
+      while(cpyN > 1){
+        facN *=(cpyN -1);
+        cpyN--;
+      }
+      while(NsubR > 1){
+        facNsubR *= (NsubR -1);
+        NsubR--;
+      }
+      this.value = facN / facNsubR;
+    }
+  }
+   reset(){
+     this.n=1;
+     this.r=1;
+   }
 }
--- recursiveApp/src/app/app.module.ts
+++ recursiveApp/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JsonFilesService } from './app.service';
+import { HttpClientModule } from '@angular/common/http';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatExpansionModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [JsonFilesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
